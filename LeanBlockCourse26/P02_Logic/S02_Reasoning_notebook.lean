d48d7c6802f79806a19eeda73d9e68b4584e8801
Théo Tyburn <theo.tyburn@posteo.net>
2026-03-04 11:23:05 +0100

    Reasoning Exercises B02
    Acked-by: Théo Tyburn <theo.tyburn@posteo.net>

--- LeanBlockCourse26/P02_Logic/S02_Reasoning_notebook.lean
+++ LeanBlockCourse26/P02_Logic/S02_Reasoning_notebook.lean
@@ -65,3 +65,48 @@ example (A B C D E F G H I : Prop)
   have hf : F := l he
   have hi : I := p hf
   exact hi
+
+
+/-
+## Exercise Block B02: Graph of Implications (Continued)
+-/
+
+-- Use only `suffices` to work backwards from the goal:
+example (A B C D E F G H I : Prop)
+    (f : A → B) (g : C → B) (h : A → D) (i : B → E) (j : C → F)
+    (k : E → D) (l : E → F) (m : G → D) (n : H → E) (p : F → I)
+    (q : H → G) (r : H → I) (a : A) : I := by
+  suffices F by
+    apply p this
+  suffices E by
+    apply l this
+  suffices B by
+    apply i this
+  suffices A by
+    apply f this
+  exact a
+
+  
+-- Use only `refine` to work backwards from the goal:
+example (A B C D E F G H I : Prop)
+    (f : A → B) (g : C → B) (h : A → D) (i : B → E) (j : C → F)
+    (k : E → D) (l : E → F) (m : G → D) (n : H → E) (p : F → I)
+    (q : H → G) (r : H → I) (a : A) : I := by
+  refine p ?_
+  refine l ?_
+  refine i ?_
+  refine f ?_
+  exact a
+
+-- Combine all of `clear`, `exact`, `have`, `suffices`, `refine`, and `apply`
+example (A B C D E F G H I : Prop)
+    (f : A → B) (g : C → B) (h : A → D) (i : B → E) (j : C → F)
+    (k : E → D) (l : E → F) (m : G → D) (n : H → E) (p : F → I)
+    (q : H → G) (r : H → I) (a : A) : I := by
+  clear g h j k m n q r
+  apply p
+  suffices E by
+    apply l this
+  refine i ?_
+  have b : B := f a
+  exact b
